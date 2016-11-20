6c2e62ad7b0b0d6ec579b5e96c646801288f7677
Karol Gil <karolgil.kg@gmail.com>
2016-11-20 20:38:48 +0100

    Removed some memos

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,16 +1,7 @@
 defmodule Cards do
 
-
   def create_deck do
     ["Ace", "Two", "Three"]
   end
-
-  # def hello do
-  #   # return "hi there!" does the same
-  #   "hi there!"
-  # end
-
-  #iex -S mix <- compiles
-  #recompile in iex <- recompiles in place
 
 end
