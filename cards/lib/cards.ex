3e6857234ec732603a0cd3afb6fb2c52ecad64c3
Karol Gil <karolgil.kg@gmail.com>
2016-11-20 20:37:17 +0100

    Course started

---
+++ cards/lib/cards.ex
@@ -0,0 +1,16 @@
+defmodule Cards do
+
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  # def hello do
+  #   # return "hi there!" does the same
+  #   "hi there!"
+  # end
+
+  #iex -S mix <- compiles
+  #recompile in iex <- recompiles in place
+
+end
